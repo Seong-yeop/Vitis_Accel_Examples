7c85f03b253d8e0a0b4770dbbcd07d1f64f746ce
Heejae Kim <adpp00@snu.ac.kr>
2024-11-04 09:51:31 +0000

    Host<->FPGA copy code

--- performance/host_memory_bandwidth_xrt/src/host.cpp
+++ performance/host_memory_bandwidth_xrt/src/host.cpp
@@ -24,6 +24,15 @@
 #include "experimental/xrt_device.h"
 #include "experimental/xrt_kernel.h"
 
+
+#include <fcntl.h>
+#include <fstream>
+#include <iomanip>
+#include <iosfwd>
+#include <unistd.h>
+
+
+
 int main(int argc, char* argv[]) {
     // Command Line Parser
     sda::utils::CmdLineParser parser;
@@ -32,25 +41,38 @@ int main(int argc, char* argv[]) {
     //**************//"<Full Arg>",  "<Short Arg>", "<Description>", "<Default>"
     parser.addSwitch("--xclbin_file", "-x", "input binary file string", "");
     parser.addSwitch("--device_id", "-d", "device index", "0");
+    parser.addSwitch("--file_path", "-p", "file path string", "");
     parser.parse(argc, argv);
 
     // Read settings
     std::string binaryFile = parser.value("xclbin_file");
     int device_index = stoi(parser.value("device_id"));
+    std::string filepath = parser.value("file_path");
+    std::string filename;
 
-    if (argc < 3) {
+    if (argc < 5) {
         parser.printHelp();
         return EXIT_FAILURE;
     }
+    
 
     std::cout << "Open the device" << device_index << std::endl;
     auto device = xrt::device(device_index);
+
     std::cout << "Load the xclbin " << binaryFile << std::endl;
     auto uuid = device.load_xclbin(binaryFile);
 
     auto krnl = xrt::kernel(device, uuid, "bandwidth");
     auto krnl_read = xrt::kernel(device, uuid, "read_bandwidth");
     auto krnl_write = xrt::kernel(device, uuid, "write_bandwidth");
+
+    if (filepath.empty()) {
+	filename = parser.value("input_file");
+    } else {
+	std::cout << "\nWARNING: Ignoring -f option when -p options is set. -p has high precedence over -f.\n";
+	filename = filepath;
+    }							    
+
 
     double concurrent_max = 0;
     double read_max = 0;
@@ -78,8 +100,13 @@ int main(int argc, char* argv[]) {
         }
 
         xrt::bo::flags flags = xrt::bo::flags::host_only;
+	    xrt::bo::flags device_flags = xrt::bo::flags::device_only;
+	
         auto hostonly_bo_in = xrt::bo(device, bufsize, flags, krnl.group_id(0));
         auto hostonly_bo_out = xrt::bo(device, bufsize, flags, krnl.group_id(1));
+
+        auto deviceonly_bo = xrt::bo(device, bufsize, device_flags, krnl.group_id(1));
+
 
         double dbytes = bufsize;
         std::string size_str = xcl::convert_size(bufsize);
@@ -97,27 +124,41 @@ int main(int argc, char* argv[]) {
         }
 
         auto start = std::chrono::high_resolution_clock::now();
-        auto run = krnl(hostonly_bo_in, hostonly_bo_out, bufsize, iter);
+        auto run = krnl(hostonly_bo_in, deviceonly_bo, bufsize, iter);
         run.wait();
         auto end = std::chrono::high_resolution_clock::now();
         double duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
         double msduration = duration / iter;
+        double dsduration = msduration / ((double)1000000);
+        double bpersec = (dbytes / dsduration);
+        double gbpersec = (bpersec) / ((double)1024 * 1024 * 1024); // For Concurrent Read and Write
 
+        std::cout << "Host -> FPGA throughput " << gbpersec << " (GB/sec) for buffer size " << size_str
+		<< std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
+        run = krnl(deviceonly_bo, hostonly_bo_out, bufsize, iter);
+	    run.wait();
+        end = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+        msduration = duration / iter;
+        dsduration = msduration / ((double)1000000);
+        bpersec = (dbytes / dsduration);
+        gbpersec = (bpersec) / ((double)1024 * 1024 * 1024); // For Concurrent Read and Write
         // Validate our results
         if (std::memcmp(bo_out_map, bo_in_map, bufsize))
             throw std::runtime_error("Value read back does not match reference");
-
+	
         /* Profiling information */
-        double dsduration = msduration / ((double)1000000);
-        double bpersec = (dbytes / dsduration);
-        double gbpersec = (2 * bpersec) / ((double)1024 * 1024 * 1024); // For Concurrent Read and Write
 
-        std::cout << "Concurrent Read and Write Throughput = " << gbpersec << " (GB/sec) for buffer size " << size_str
-                  << std::endl;
+        std::cout << "FPGA -> Host throughput " << gbpersec << " (GB/sec) for buffer size " << size_str
+        		<< std::endl;
 
         if (gbpersec > concurrent_max) {
             concurrent_max = gbpersec;
         }
+
+	    continue;
 
         start = std::chrono::high_resolution_clock::now();
         auto run_read = krnl_read(hostonly_bo_in, bufsize, iter);
@@ -158,8 +199,8 @@ int main(int argc, char* argv[]) {
 
     std::cout << "Maximum bandwidth achieved :\n";
     std::cout << "Concurrent Read and Write Throughput = " << concurrent_max << " (GB/sec) \n";
-    std::cout << "Read Throughput = " << read_max << " (GB/sec) \n";
-    std::cout << "Write Throughput = " << write_max << " (GB/sec) \n\n";
+//    std::cout << "Read Throughput = " << read_max << " (GB/sec) \n";
+//    std::cout << "Write Throughput = " << write_max << " (GB/sec) \n\n";
     std::cout << "TEST PASSED\n";
     return EXIT_SUCCESS;
 }
